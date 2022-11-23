f93fa8efcc432d84d994d16291d0afdc7cfd0f1d
slavcilla <118916906+slavcilla@users.noreply.github.com>
2022-11-23 18:53:19 +0300

    Add files via upload
    Тест

---
+++ test.php
@@ -0,0 +1,7 @@
+<?php include('wp-blog-header.php'); ?>
+<?php get_header(); ?>
+<!Ц –азмещаем здесь контент в HTML или PHP Ц>
+Е
+<!Ц  онец секции кода страницы Ц>
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
